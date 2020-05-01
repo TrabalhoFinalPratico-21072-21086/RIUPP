0015b9498294db9772c4fcca86feb8d47e825856
JoaoVerissimo <joao17andre@hotmail.com>
2020-05-01 11:57:06 +0100

    Atualização das Classes

---
+++ RIUPP/RIUPP/Models/Area.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RIUPP.Models{
+    public class Area{
+        [Key]
+        public int id { get; set; }
+        public String nome { get; set; }
+        public String designacao { get; set; }
+
+    }
+}
---
+++ RIUPP/RIUPP/Models/Comentario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RIUPP.Models{
+    public class Comentario{
+
+        [Key]
+        public int id { get; set; }
+        public String comentario { get; set; }
+        public String visivel { get; set; }
+        public DateTime date { get; set; }
+    }
+}
